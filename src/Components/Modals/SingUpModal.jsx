fe57b60dcb450f27d3da0e5338d8ade338f2c9b4
Hayk Kirakosyan <hayk.kirakosyan101@gmail.com>
2024-03-29 10:46:04 +0400

    I corrected all the changes, the page is almost done

--- src/Components/Modals/SingUpModal.jsx
+++ src/Components/Modals/SingUpModal.jsx
@@ -5,25 +5,16 @@ import {Input,FormLabel,FormControl,Box, Button, Center, Flex ,Image, useDisclos
 const NAME_PATTERN = /^[a-z ,.'-]+$/i;
 const AGE_PATTERN = /^\S[0-9]{0,3}$/;
 const PASSWORD_PATTERN = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
-const EMAIL_PATTERN = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit}){
+const EMAIL_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export default function SingUpModal({isSingUpOpen,onSingUpClose,handleSubmit}){
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
     const [age,setAge] = useState("")
     const [name,setName] = useState("")
-    function handleEmailValue(e){
-        setEmail(e.target.value)
-    }
-    function handlePasswordValue(e){
-        setPassword(e.target.value)
-    }
-    function handleAgeValue(e){
-        setAge(e.target.value)
-    }
-    function handleNameValue(e){
-        setName(e.target.value)
-
-    }
+    const handleEmailChange = (e) => setEmail(e.target.value)
+    const handlePasswordChange = (e) => setPassword(e.target.value)
+    const handleAgeChange = (e) => setAge(e.target.value)
+    const handleNameChange = (e) => setName(e.target.value)
     const validateEmail = (email) => (EMAIL_PATTERN.test(email))
     const validatePassword = (password) => (PASSWORD_PATTERN.test(password))
     const validateName = (name) => (NAME_PATTERN.test(name))
@@ -35,10 +26,10 @@ export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit
                         <ModalHeader>Sing Up</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody>
-                            <form onSubmit={handleFormSubmit}>
+                            <form onSubmit={handleSubmit}>
                             <FormControl isRequired isInvalid={ name.length && !validateName(name)}>
                                 <FormLabel>Name</FormLabel>
-                                <Input  type="text" value={name} onChange={handleNameValue}/>
+                                <Input  type="text" value={name} onChange={handleNameChange}/>
                                 {name.length && !validateName(name) ? (
                                     <FormErrorMessage>Incorrect</FormErrorMessage>
                                     ) : (
@@ -49,7 +40,7 @@ export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit
                             </FormControl>
                             <FormControl isRequired isInvalid={password.length && !validatePassword(password)}>
                                 <FormLabel>Password</FormLabel>
-                                <Input type='password' value={password} onChange={handlePasswordValue}/>
+                                <Input type='password' value={password} onChange={handlePasswordChange}/>
                                 {password.length && !validatePassword(password) ? (
                                     <FormErrorMessage>Incorrect</FormErrorMessage>
                                 
@@ -61,7 +52,7 @@ export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit
                             </FormControl>
                             <FormControl isRequired isInvalid={age.length && !validateAge(age)}>
                                 <FormLabel>Age</FormLabel>
-                                <Input type='text' value={age} onChange={handleAgeValue}/>
+                                <Input type='text' value={age} onChange={handleAgeChange}/>
                                 {age.length && !validateAge(age) ? (
                                     <FormErrorMessage>Incorrect</FormErrorMessage>
                                 
@@ -73,7 +64,7 @@ export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit
                             </FormControl>
                             <FormControl isRequired isInvalid={email.length && !validateEmail(email)}>
                                 <FormLabel>Email</FormLabel>
-                                <Input type='email' value={email} onChange={handleEmailValue}/>
+                                <Input type='email' value={email} onChange={handleEmailChange}/>
                                 {email.length && !validateEmail(email) ? (
                                     <FormErrorMessage>Incorrect</FormErrorMessage>
                                 
@@ -87,7 +78,7 @@ export default function SingUpModal({isSingUpOpen,onSingUpClose,handleFormSubmit
                 
                         </ModalBody>
                         <ModalFooter>
-                            <Button colorScheme='blue' mr={3} onClick={handleFormSubmit} isDisabled={!validateAge(age) || !validateEmail(email) || !validateName(name) || !validatePassword(password)}>
+                            <Button colorScheme='blue' mr={3} onClick={handleSubmit} isDisabled={!validateAge(age) || !validateEmail(email) || !validateName(name) || !validatePassword(password)}>
                                 Submit
                             </Button>
                         </ModalFooter>
